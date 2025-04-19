274caacd7cb849eb6b097cd7783bf21bd0850f91
Takuya <46385936+Takuya-Miyazaki@users.noreply.github.com>
2025-04-20 08:04:42 +0900

    Update page.tsx
    Add
    
    Signed-off-by: Takuya <46385936+Takuya-Miyazaki@users.noreply.github.com>

--- app/[poll_id]/page.tsx
+++ app/[poll_id]/page.tsx
@@ -1,14 +1,1 @@
-const req = await fetch(`${PARTYKIT_URL}/party/${pollId}`, {
-  method: "GET",
-  next: {
-    revalidate: 0
-  }
-});
-
-if (!req.ok) {
-  if (req.status === 404) {
-    notFound();
-  } else {
-    throw new Error("Something went wrong.");
-  }
-}
+const poll = (await req.json()) as Poll;
